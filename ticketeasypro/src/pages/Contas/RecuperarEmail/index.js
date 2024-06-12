80f782e136a7314b9ab22c7189244a757ab8f77e
ChristianSCubo <chriscubo@gmail.com>
2024-06-12 18:24:26 -0300

    Correção das várias renderizações do token

--- ticketeasypro/src/pages/Contas/RecuperarEmail/index.js
+++ ticketeasypro/src/pages/Contas/RecuperarEmail/index.js
@@ -2,17 +2,17 @@ import React, { useEffect, useState, useCallback } from "react";
 import { useRouter } from "next/router";
 import Cabecalho from "./../../Cabecalho";
 import "bootstrap/dist/css/bootstrap.min.css";
-import EmailConfirmation from "@components/EmailConfirmation";
 import EmailResendForm from "@components/forms/EmailResendForm";
 import LoadingOverlay from "@components/LoadingOverlay";
 import styleh from "@styles/Home.module.css";
 import styles from "@styles/CadastroCliente.module.css";
 import ToastMessage from "@/components/ToastMessage/ToastMessage";
 import CabecalhoHomeMenu from "@/pages/CabecalhoHomeMenu";
+import client from "@/utils/client_axios";
 
 const RecuperarEmail = () => {
   const router = useRouter();
-  const { token } = router.query;
+  const token = router.query.token;
   const [loading, setLoading] = useState(false);
   const [message, setMessage] = useState({ text: "", type: "" });
 
@@ -20,11 +20,66 @@ const RecuperarEmail = () => {
     setMessage({ text: message, type });
   }, []);
 
+  // Mensagens de erro
+  const handleError = (response) => {
+    if (response?.data?.message) {
+      const messageError = response.data.message;
+      switch (messageError) {
+        case "EmailAlreadyConfirmed":
+          handleSetMessage("Email já confirmado. Faça seu login.", "error");
+          setTimeout(() => router.push("/Login"), 3000);
+          break;
+        case "InvalidOrExpiredToken":
+          handleSetMessage("Seu email de confirmação expirou.", "error");
+          setTimeout(() => router.push("/Contas/RecuperarEmail"), 3000);
+          break;
+        default:
+          console.log(response);
+          handleSetMessage("Erro desconhecido", "error");
+      }
+    } else {
+      handleSetMessage(
+        "Falha ao validar email. Por favor tente novamente.",
+        "error"
+      );
+    }
+  };
+
+  // Função para validar o token
+  const validarToken = async () => {
+    setLoading(true);
+    try {
+      const response = await client.get(
+        `accounts/confirm-email?token=${encodeURIComponent(token)}`
+      );
+      if (response.status === 200) {
+        handleSetMessage("Email confirmado!", "success");
+        setTimeout(() => router.push("/Login"), 1750);
+      } else {
+        handleError(response);
+      }
+    } catch (error) {
+      console.error("Erro na requisição para confirmar token:", error);
+      handleError(error.response);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  // Função para o efeito de carregamento
   useEffect(() => {
     if (loading) {
       setMessage({ text: "", type: "" });
     }
+    console.log("loading");
   }, [loading]);
+
+  // Função para validar o token quando existir
+  useEffect(() => {
+    if (token) {
+      validarToken();
+    }
+  }, [token]);
 
   const setLoadingWithDelay = useCallback((isLoading) => {
     setLoading(isLoading);
@@ -37,13 +92,6 @@ const RecuperarEmail = () => {
       <div className={styles.divPrincipal_recuperar_email}>
         <div className="div_container_grande">
           {loading && <LoadingOverlay />}
-          {!loading && token && (
-            <EmailConfirmation
-              token={token}
-              handleSetMessage={handleSetMessage}
-              setLoading={setLoadingWithDelay}
-            />
-          )}
           {!loading && !token && (
             <>
               <div className="text-center">
