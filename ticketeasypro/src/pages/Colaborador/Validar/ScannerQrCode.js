9676a8dba6daf28056635a8247d089d3811c14f6
Luiz Otavio de Oliveira Nogueira <luiz_loon@yahoo.com.br>
2024-06-28 09:32:54 -0300

    fix(scannerqrcode): ajuste mesagens de retorno

--- ticketeasypro/src/pages/Colaborador/Validar/ScannerQrCode.js
+++ ticketeasypro/src/pages/Colaborador/Validar/ScannerQrCode.js
@@ -6,36 +6,22 @@ import seta from "../../../assets/seta para cima.jpg";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { Html5QrcodeScanner } from "html5-qrcode";
 import client from "@/utils/client_axios";
-import { parseCookies } from "nookies";
+import { getToken } from "@/utils";
+
 import ToastMessage from "@/components/ToastMessage/ToastMessage";
 
-function getToken() {
-  const cookies = parseCookies();
-  let token;
-  let valorToken;
-  if (cookies && cookies["ticket-token"]) {
-    token = cookies["ticket-token"]; // Assumindo que o nome do cookie é 'ticket-token'
-    valorToken = JSON.parse(token);
-  }
-  return valorToken;
-}
-
-export default function ScannerQrCode() {
+const ScannerQrCode = () => {
   const router = useRouter();
   const [botao, setBotao] = useState(false);
   const [idIngresso, setIdIngresso] = useState("");
   const [message, setMessage] = useState({ text: "", type: "" });
 
-  const handleSetMessage = (message, type) => {
-    ({ text: message, type });
-  };
-
   const setErrorMessage = useCallback(
-    (message) => setMessage(message, "error"),
+    (message) => setMessage({ text: message, type: "error" }),
     []
   );
   const setSuccessMessage = useCallback(
-    (message) => setMessage(message, "success"),
+    (message) => setMessage({ text: message, type: "success" }),
     []
   );
 
@@ -45,10 +31,8 @@ export default function ScannerQrCode() {
         width: 250,
         height: 250,
       },
-      fps: 100,
+      fps: 10,
     });
-
-    scanner.render(success, error);
 
     function success(result) {
       scanner.pause();
@@ -59,17 +43,15 @@ export default function ScannerQrCode() {
       if (isUUID4) {
         ValidarCodigo(result);
       } else {
-        handleSetMessage(
-          "Não foi encontrado o ingresso com este QR Code.",
-          "error"
-        );
+        setErrorMessage("Não foi encontrado o ingresso com este QR Code.");
       }
     }
 
     function error(err) {
-      scanner.pause();
       console.warn(err);
     }
+
+    scanner.render(success, error);
 
     return () => {
       scanner.clear();
@@ -85,11 +67,19 @@ export default function ScannerQrCode() {
       const response = await client.get(`tickets/${idIngresso}/checkin`, {
         headers: { Authorization: `Bearer ${getToken()?.accessToken}` },
       });
+      if (response.status === 404) {
+        setErrorMessage("Ingresso não existe.");
+        return;
+      }
 
-      if (response == 200) {
-        if (response?.message?.fail) {
-          const mensagem = response?.message?.fail;
+
+      if (response.status === 200) {
+        const mensagem = response?.data?.fail;
+        if (mensagem) {
           switch (mensagem) {
+            case "TicketReservedPendingPayment":
+              setErrorMessage("O ingresso/ordem de compra precisa ser pago.");
+              return;
             case "TicketHasBeenUsed":
               setErrorMessage("Esse ingresso já foi usado.");
               return;
@@ -100,12 +90,7 @@ export default function ScannerQrCode() {
               setErrorMessage("Esse ingresso está expirado.");
               return;
           }
-        }
-        if (response?.message == "TicketReservedPendingPayment") {
-          setErrorMessage("O ingresso/ordem de compra precisa ser pago.");
-          return;
-        }
-
+        };
         setSuccessMessage("Ingresso validado com sucesso!");
         setTimeout(() => {
           router.back();
@@ -115,6 +100,7 @@ export default function ScannerQrCode() {
       setErrorMessage("Falha ao validar o ticket. Erro inesperado.");
     }
   };
+
   return (
     <div className={styles.body_qr_code}>
       <div id="qrcode" className={styles.qrcode_scanner}></div>
@@ -145,9 +131,7 @@ export default function ScannerQrCode() {
                 />
               </div>
             </div>
-          ) : (
-            <></>
-          )}
+          ) : null}
         </div>
       </div>
 
@@ -157,3 +141,4 @@ export default function ScannerQrCode() {
     </div>
   );
 }
+export default ScannerQrCode;
